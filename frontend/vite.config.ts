112e9c170246746e0d424a8b49cf6a95b105066a
Sonya <preminina.sofya@gmail.com>
2026-02-09 01:48:15 +0300

    refactor: move URLs to .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    
-    port: 5173,
-    allowedHosts: ['early-pets-retire.loca.lt'],
-    proxy: {
-      '/api': {
-        target: "https://habittasktracker-sysliks29.amvera.io/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const allowedHosts = env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',').map(host => host.trim()) : [];
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'build',
+    },
+    server: {
+      
+      port: 5173,
+      allowedHosts: allowedHosts,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
